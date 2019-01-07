9e3eee0e80b3dd4411e09c4b779e11901c1aeb96
tainguyennls <nhuutai2698@gmail.com>
2019-01-07 18:32:59 +0700

    .

--- app/src/main/java/com/example/nguyenhuutai/studentapp/models/CheckNetwork.java
+++ app/src/main/java/com/example/nguyenhuutai/studentapp/models/CheckNetwork.java
@@ -1,5 +1,25 @@
 package com.example.nguyenhuutai.studentapp.models;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 public class CheckNetwork {
+
+    public static final int NO_CONNECT_NETWORK = 0;
+    public static final int UNKNOWN_CONNECT_TYPE = -1;
+
+    public CheckNetwork(){
+
+    }
+    public int getNetworkType(Context context){
+        ConnectivityManager connectivityManager  = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if(networkInfo == null || !networkInfo.isConnected()){
+            return  NO_CONNECT_NETWORK;
+        }
+        return 0; // only for demo...
+    }
 
 }
